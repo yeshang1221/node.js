2ad54f6d92b9a0195097cbf3d2f877a37e6e8d3a
claire <c1994831221@gmail.com>
2021-05-23 09:02:37 +0800

    add file

---
+++ basic/sum.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+
+let name="yeshang";
+console.log(name);
+
+
+function sum(s){
+    let total=0
+    for(i=0;i<=s;i++){
+        total+=i
+    }
+    return total
+}
+console.log(sum(1)); 
+console.log(sum(10));
+
