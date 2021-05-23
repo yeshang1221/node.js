1dcbd9089c2ccdfacd2f6b688eeb875276d9ad70
claire <c1994831221@gmail.com>
2021-05-23 14:41:37 +0800

    update

--- basic/quiz.js
+++ basic/quiz.js
@@ -1,4 +1,4 @@
-(1) 請問下列程式執行後的結果為何？為什麼？
+
 
 console.log("start");
 
@@ -10,7 +10,6 @@ console.log("start");
 })();
 
 console.log("end");
-(2) 請問下列程式執行的結果為何？為什麼？
 
 console.log("start");
 
@@ -22,7 +21,7 @@ console.log("start");
 })();
 
 console.log("end");
-(3) 請問下列程式執行的結果為何？為什麼？
+
 
 const bar = () => console.log("bar");
 
@@ -35,7 +34,7 @@ const foo = () => {
 };
 
 foo();
-(4) 請問下列程式執行的結果為何？為什麼？
+
 
 const bar = () => console.log("bar");
 
