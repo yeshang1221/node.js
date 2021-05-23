9b094c00fa87ac7a473e3fda042b6ba8de1f22b6
claire <c1994831221@gmail.com>
2021-05-23 17:41:59 +0800

    update

--- basic/quiz.js
+++ basic/quiz.js
@@ -1,39 +1,39 @@
 
 
-console.log("start");
+// console.log("start");
 
-(function () {
-  console.log("IIFE");
-  setTimeout(function () {
-    console.log("Timeout");
-  }, 1000);
-})();
+// (function () {
+//   console.log("IIFE");
+//   setTimeout(function () {
+//     console.log("Timeout");
+//   }, 1000);
+// })();
 
-console.log("end");
+// console.log("end");
 
-console.log("start");
+// console.log("start");
 
-(function () {
-  console.log("IIFE");
-  setTimeout(function () {
-    console.log("Timeout");
-  }, 0);
-})();
+// (function () {
+//   console.log("IIFE");
+//   setTimeout(function () {
+//     console.log("Timeout");
+//   }, 0);
+// })();
 
-console.log("end");
+// console.log("end");
 
 
-const bar1 = () => console.log("bar");
+const bar1 = () => console.log("bar1");
 
-const baz = () => console.log("baz");
+const baz1 = () => console.log("baz1");
 
-const foo = () => {
-    console.log("foo");
+const foo1 = () => {
+    console.log("foo1");
     bar1();
-    baz();
+    baz1();
 };
 
-foo();
+foo1();
 
 
 const bar = () => console.log("bar");
