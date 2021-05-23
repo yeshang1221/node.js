e37734f14a58322a5519b39caa898eba964aab59
claire <c1994831221@gmail.com>
2021-05-23 09:30:47 +0800

    update

--- basic/sum.js
+++ basic/sum.js
@@ -6,12 +6,23 @@ console.log(name);
 
 
 function sum(s){
-    let total=0
-    for(i=0;i<=s;i++){
-        total+=i
+    let total = 0;
+    for(let i = 0 ;i <= s; i++ ){
+        total ++;
     }
-    return total
+    return total;
 }
 console.log(sum(1)); 
 console.log(sum(10));
-
+// 老師的寫法(在這邊這樣)
+function sum(n){
+    // 0(1);
+    // return ((1+n)*n)/2;
+    let sum = 0;
+    for(let i=1;i<=n;i++){
+    sum+=i;
+    }
+    return sum;
+}
+console.log(sum(10))
+//--------------------
