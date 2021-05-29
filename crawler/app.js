93deb43fb79b848fbc11709f3bc5b04a567eb51d
claire <c1994831221@gmail.com>
2021-05-29 12:20:13 +0800

    update

--- crawler/app.js
+++ crawler/app.js
@@ -1,1 +1,18 @@
-123
+const axios =require('axios');
+
+axios.get('https://www.twse.com.tw/exchangeReport/STOCK_DAY?',{
+    params: {
+        response:"json",
+        date:"20210529",
+        stockNo:"0050",
+    },
+})
+.then(function(response){
+    console.log(response);
+})
+.catch(function(error){
+    console.log(error);
+})
+.then(function(){
+    //always executed
+})
