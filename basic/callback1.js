121aa252afed3c195d1de9151450163aac2e1c9a
claire <c1994831221@gmail.com>
2021-05-29 15:52:43 +0800

    update

---
+++ basic/callback1.js
@@ -0,0 +1,59 @@
+  
+let doWork = function (job, timer, cb) {
+    setTimeout(() => {
+      let dt = new Date();
+      cb(null, `完成工作: ${job} at ${dt.toISOString()}`);
+    }, timer);
+  };
+  
+  let dt = new Date();
+  console.log(`開始工作 at ${dt.toISOString()}`);
+  doWork("刷牙", 2000, function (err, result) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(result);
+    doWork("吃早餐", 3000, function (err, result) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log(result);
+        doWork("寫功課", 3000, function (err, result) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(result);
+          });
+      });
+  });
+// 上面同步 下面非同步
+
+//   doWork("刷牙", 2000, function (err, result) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(result);
+// });
+// doWork("吃早餐", 3000, function (err, result) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(result);
+// });
+// doWork("寫功課", 3000, function (err, result) {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//     console.log(result);
+//   });
+  
+  
+ 
+  
+
